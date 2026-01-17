3076a59ffc2749f7ffc06b350bf0751ca9fe9acd
tainasts <tainaroberta102@gmail.com>
2026-01-17 12:30:03 -0300

    em andamento

---
+++ reveal.js
@@ -0,0 +1,63 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO do SITE
+revelar.reveal('.efeito-txt-topo',{
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',{
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+});
+
+// Titulos da sessões
+revelar.reveal('.title-paragraph',{
+  duration: 2000,
+  distance: '90px'
+});
+
+// Sessão acompanhamento
+revelar.reveal('.efeito-acompanhamento-1',{
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+});
+
+revelar.reveal('.efeito-acompanhamento-2',{
+  duration: 2000,
+  distance: '90px',
+  delay: 1000
+});
+
+revelar.reveal('.efeito-acompanhamento-3',{
+  duration: 2000,
+  distance: '90px',
+  delay: 1500
+});
+
+// Sessão sintomas
+revelar.reveal('.card-section1',{
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+});
+
+revelar.reveal('.card-section2',{
+  duration: 2000,
+  distance: '90px',
+  delay: 1000
+});
+
+revelar.reveal('.card-section3',{
+  duration: 2000,
+  distance: '90px',
+  delay: 1500
+});
+
+revelar.reveal('.card-section4',{
+  duration: 2000,
+  distance: '90px',
+  delay: 2000
+});
